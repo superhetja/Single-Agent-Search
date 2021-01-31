0834ba1910b582cd07f70ffd946286ba75c79364
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-01-31 19:49:02 +0000

    commented out the test

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,17 @@ public class Main {
 	 */
 	public static void main(String[] args){
 		try{
+			// TODO: choose a different search method and/or heuristics here
+			SearchAlgorithm search = new AStarSearch(new SimpleHeuristics()); 
+			Agent agent = new VaccuumCleanerAgent(search);
+
+			int port=4001;
+			if(args.length>=1){
+				port=Integer.parseInt(args[0]);
+			}
+			GamePlayer gp=new GamePlayer(port, agent);
+			gp.waitForExit();
+			/*
 			//============ testing getNextState() ============
 
 			// initialize
@@ -75,18 +86,8 @@ public class Main {
 			env.currentState.position = new Coordinates(1, 1);
 			if((env.getNextState(env.currentState, Action.SUCK).equals(env.currentState))){
 				System.out.println("Incorrect Suck, should have removed some dirt");
-			}
-/*
-			// TODO: choose a different search method and/or heuristics here
-			SearchAlgorithm search = new AStarSearch(new SimpleHeuristics()); 
-			Agent agent = new VaccuumCleanerAgent(search);
+			}*/
 
-			int port=4001;
-			if(args.length>=1){
-				port=Integer.parseInt(args[0]);
-			}
-			GamePlayer gp=new GamePlayer(port, agent);
-			gp.waitForExit();*/
 		}catch(Exception ex){
 			ex.printStackTrace();
 			System.exit(-1);
